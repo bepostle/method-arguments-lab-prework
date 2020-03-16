7caf9b1639bc2f50bef1bc6842dbf533d9f3d625
Brent Postlethweight <brentpostle@gmail.com>
2020-03-16 22:21:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,4 +8,4 @@ def introduction_with_language 'name', 'language'
   puts "Hi #{name}, you are a good #{language} programmer."
 end
 
-introduction_with_language
+introduction_with_language 'Brent', 'Ruby'
